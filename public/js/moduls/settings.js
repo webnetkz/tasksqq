39bcc4e103fab1e964fad925a0c672bd15d6262f
webnetkz <dbqqbq@gmail.com>
2021-12-31 23:13:35 +0600

    create taskBoard module

--- public/js/moduls/settings.js
+++ public/js/moduls/settings.js
@@ -7,5 +7,21 @@ export function initSettings() {
         initTaskBoard();
     } else {
         initTaskBoard();
+        getTaskBoards();
+    }
+}
+
+function getTaskBoards() {
+    let taskBoards = localStorage.getItem('task_boards');
+    taskBoards = JSON.parse(taskBoards);
+
+    let ulTaskBoards = document.querySelector('menu > div > ul');
+    for(let i = 0; i < taskBoards.length; i++) {
+        let tmp = document.createElement('li');
+        tmp.classList.add('board');
+        tmp.innerText = taskBoards[i];
+
+        ulTaskBoards.appendChild(tmp);
+        ulTaskBoards.innerHTML += `<li class="board" id="createTaskBoard">+ Добавить список</li>`;
     }
 }
--- public/js/moduls/taskBoard.js
+++ public/js/moduls/taskBoard.js
@@ -8,20 +8,5 @@ export function initTaskBoard() {
     localStorage.setItem('task_boards', taskBoards);
 
     console.log(activeTaskBoard);
-    getTaskBoards();
 }
 
-function getTaskBoards() {
-    let taskBoards = localStorage.getItem('task_boards');
-    taskBoards = JSON.parse(taskBoards);
-
-    let ulTaskBoards = document.querySelector('menu > div > ul');
-    for(let i = 0; i < taskBoards.length; i++) {
-        let tmp = document.createElement('li');
-        tmp.classList.add('board');
-        tmp.innerText = taskBoards[i];
-
-        ulTaskBoards.appendChild(tmp);
-        ulTaskBoards.innerHTML += `<li class="board" id="createTaskBoard">+ Добавить список</li>`;
-    }
-}
