2df8f936cf9205d87ea5232d376993eb49d8e5ff
webnetkz <dbqqbq@gmail.com>
2022-01-02 19:49:50 +0600

    update fetch module

--- public/js/moduls/fetch.js
+++ public/js/moduls/fetch.js
@@ -1,34 +1,32 @@
+/*
+    Модуль fetch запросов
+    v0.5
+*/
+// Запрос с настройками метода и отправкой тела в запросе
 export async function f(url, type='text', req='', meth='POST') {
-    let data;
+    let data; // Данные которые выдаст функция в итоге
 
-    let res = await fetch(url, {
-        method: meth,
-        headers: {
+    let res = await fetch(url, { // Сам запрос на URL
+        method: meth, // Метод запроса POST/PUT/DELETED
+        headers: { // Заголовки
             'Content-Type': 'application/json'
         },
-        body: req
+        body: req // Тело запроса
     });
 
-    if(res.ok) {
-        if(type == 'text') {
-            data = await res.text();
+    if(res.ok) { // Если ответ 200
+        if(type == 'text') { 
+            data = res.text(); // Получаем текстовый ответ
         }
-        if(type == 'json') {
-            data = await res.text();
+        if(type == 'json') { 
+            data = res.text(); // Получаем json ответ
         }
     }
     return data;
 }
 
-export function fG(url, meth='GET') {
-    fetch(url, {
-        method: meth,
-        headers: {
-            'Content-Type': 'application/json'
-        },
-    })
-    .then(res => res.json())
-    .then(res => {
-        return res;
-    })
+// GET запрос, ответ в json формате
+export async function fG(url) {
+    const tmp = await fetch(url)
+    return tmp.json();
 }
